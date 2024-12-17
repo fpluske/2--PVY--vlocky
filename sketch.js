f1897a34583b4d19c5d5aa01d0325e0c49507dfa
fpluske <frantisek.pluske@seznam.cz>
2024-12-17 11:57:48 +0100

    V 0.1

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let snowflakes = [];
+
+class Snowflake {
+  constructor() {
+    this.x = random(width);
+    this.y = 0;
+    this.size = random(10, 20);
+    this.speed = random(1, 3);
+    this.color = color(random(100, 255));
+  }
+
+  draw() {
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+  }
+
+  update() {
+    this.y += this.speed;
+  }
+}
+
+function setup() {
+  createCanvas(850, 700);
+}
+
+function draw() {
+  background(220);
+  if (random(1) < 0.2) snowflakes.push(new Snowflake());
+  for (let i = 0; i < snowflakes.length; i++) {
+    if (snowflakes[i].y > height + 20) {
+      snowflakes.splice(i, 1);
+    }
+    snowflakes[i].update();
+    snowflakes[i].draw();
+  }
+}
