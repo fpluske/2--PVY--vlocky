b417f58031b5e30870ef86f6e481d7269e17559f
fpluske <frantisek.pluske@seznam.cz>
2024-12-19 11:47:40 +0100

    V 0.4

--- sketch.js
+++ sketch.js
@@ -12,6 +12,8 @@ class Rectangle {
     this.y = y;
     this.width = 50;
     this.height = 30;
+    this.speed = 5;
+    this.points = 0;
     this.color = color(random(255), random(255), random(255));
   }
 
@@ -23,8 +25,12 @@ class Rectangle {
   }
 
   move(dx, dy) {
-    this.x += dx;
-    this.y += dy;
+    this.x = constrain(this.x + dx * this.speed, 0, width - this.width);
+    this.y = constrain(this.y + dy * this.speed, 0, height - this.height);
+  }
+
+  detectCollision(snowflake) {
+    return collideRectCircle(this.x, this.y, this.width, this.height, snowflake.x, snowflake.y, snowflake.size);
   }
 }
 
@@ -33,7 +39,7 @@ class Snowflake {
     this.x = random(width);
     this.y = 0;
     this.size = random(10, 20);
-    this.speed = random(1, 3);
+    this.speed = random(1.5, 5);
     this.color = color(random(100, 255));
   }
 
@@ -72,12 +78,31 @@ function draw() {
   }
   if (random(1) < 0.2) snowflakes.push(new Snowflake());
   for (let i = 0; i < snowflakes.length; i++) {
+    if (pokusny.detectCollision(snowflakes[i])) {
+      pokusny.points++;
+      snowflakes.splice(i, 1);
+      console.log(pokusny.points);
+      if (dingSound && dingSound.isLoaded()) {
+        dingSound.play();
+      }
+    }
     if (snowflakes[i].y > height + 20) {
       snowflakes.splice(i, 1);
     }
     snowflakes[i].update();
     snowflakes[i].draw();
   }
-  pokusny.move(-1, -1);
+  if (keyIsDown(LEFT_ARROW)) {
+    pokusny.move(-1, 0);
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    pokusny.move(1, 0);
+  }
+  if (keyIsDown(UP_ARROW)) {
+    pokusny.move(0, -1);
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    pokusny.move(0, 1);
+  }
   pokusny.draw();
 }
